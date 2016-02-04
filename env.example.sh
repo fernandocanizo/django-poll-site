90f08fe686c622b90cfb9bfd2d461049fc36c4e6
Fernando L. Canizo <fernandocanizo@gmail.com>
2016-02-04 20:33:40 -0300

    Start project
    - django-admin startproject poll_site
    - remove SECRET_KEY from settings.py
    - create env.example.sh
    - add git ignore file
    - add favicon
    - add basic requirements

---
+++ env.example.sh
@@ -0,0 +1,11 @@
+# Edit appropriately and rename this file to `.env`
+
+export DEBUG=True
+export SECRET_KEY='some secret hard to guess string'
+
+
+# You must have created the virtual environment prior to activate it
+. /path/to/your/virtual/environment/bin/activate
+
+# let manage.py into PATH
+export PATH="/path/to/your/project/root:${PATH}"
