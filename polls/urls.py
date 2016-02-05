0974aef0e02844842194f124e1a5955abaa6577e
Fernando L. Canizo <fernandocanizo@gmail.com>
2016-02-05 12:16:23 -0300

    Define basic routing and set mock-up views

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,34 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(
+        r'^$',
+        views.index,
+        name='polls_index'),
+
+    url(
+        r'^(?P<poll_id>\d+)$',
+        views.detail,
+        name='polls_detail'),
+
+    url(
+        r'^year/(?P<year>\d{4})$',
+        views.show_polls_from,
+        name='polls_show_polls_from'),
+
+    url(
+        r'^year/(?P<year>\d{4})/month/(?P<month>\d{2})$',
+        views.show_polls_from,
+        name='polls_show_polls_from'),
+
+    url(
+        r'^year/(?P<year>\d{4})/month/(?P<month>\d{2})/day/(?P<day>\d{2})$',
+        views.show_polls_from,
+        name='polls_show_polls_from'),
+
+    # AJAX
+    url(
+        r'^(?P<poll_id>\d+)/vote/choice/(?P<choice_id>\d+)$',
+        views.ajax_vote,
+        name="polls_ajax_vote"),
     ]
--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,25 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello")
+    return HttpResponse("Latest polls...")
+
+
+def detail(request, poll_id):
+    return HttpResponse("Shows detailed view for poll {}".format(poll_id))
+
+
+def show_polls_from(request, year, month=None, day=None):
+    return HttpResponse(
+        """Show polls from a specific date. Received:
+        year: {}
+        month: {}
+        day: {}
+        """.format(year, month, day))
+
+
+def ajax_vote(request, poll_id, choice_id):
+    return HttpResponse(
+        """This should return JSON
+        poll_id: {}
+        choice_id: {}
+        """.format(poll_id, choice_id))
