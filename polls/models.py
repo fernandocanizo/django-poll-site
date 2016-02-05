532b40d7a012db43d137661382cdf7ae3c983ed1
Fernando L. Canizo <fernandocanizo@gmail.com>
2016-02-05 11:15:39 -0300

    Create polls models, add polls app, allow admin it
    Plus migrations to get the desired model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    created_ts = models.DateTimeField()
+    updated_ts = models.DateTimeField(null=True, default=None)
+    is_published = models.BooleanField(default=False)
+    publication_date = models.DateTimeField(
+        'date published',
+        default=None,
+        )
+
+    def __str__(self):
+        return self.text
+
+    def was_published_recently(self):
+        return self.publication_date >= timezone.now() - timedelta(days=1)
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created_ts = timezone.now()
+            self.updated_ts = timezone.now()
+            return super(Question, self).save(*args, **kwargs)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
