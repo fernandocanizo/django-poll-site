b3e0a309ecd96f350ecf89c80de9d58601ad5d21
Fernando L. Canizo <fernandocanizo@gmail.com>
2016-02-05 14:02:45 -0300

    Add polls namespace to urls

--- polls/urls.py
+++ polls/urls.py
@@ -1,35 +1,37 @@
 from django.conf.urls import url
 from . import views
 
+
+app_name = 'polls'
 urlpatterns = [
     url(
         r'^$',
         views.index,
-        name='polls_index'),
+        name='index'),
 
     url(
         r'^(?P<poll_id>\d+)$',
         views.detail,
-        name='polls_detail'),
+        name='detail'),
 
     url(
         r'^year/(?P<year>\d{4})$',
         views.show_polls_from,
-        name='polls_show_polls_from'),
+        name='show_polls_from'),
 
     url(
         r'^year/(?P<year>\d{4})/month/(?P<month>\d{2})$',
         views.show_polls_from,
-        name='polls_show_polls_from'),
+        name='show_polls_from'),
 
     url(
         r'^year/(?P<year>\d{4})/month/(?P<month>\d{2})/day/(?P<day>\d{2})$',
         views.show_polls_from,
-        name='polls_show_polls_from'),
+        name='show_polls_from'),
 
     # AJAX
     url(
         r'^(?P<poll_id>\d+)/vote/choice/(?P<choice_id>\d+)$',
         views.ajax_vote,
-        name="polls_ajax_vote"),
+        name="ajax_vote"),
     ]
